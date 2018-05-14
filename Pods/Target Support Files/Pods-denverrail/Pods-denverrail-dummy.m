a6c953d65cfe540ee75e99d78bbe42d014c99a7a
Chad Francis <yamanote1138@gmail.com>
2018-05-13 21:50:11 -0600

    added Podfile w/ FMDB as dependency

---
+++ Pods/Target Support Files/Pods-denverrail/Pods-denverrail-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_denverrail : NSObject
+@end
+@implementation PodsDummy_Pods_denverrail
+@end
---
+++ Pods/Target Support Files/Pods-denverrailUITests/Pods-denverrailUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_denverrailUITests : NSObject
+@end
+@implementation PodsDummy_Pods_denverrailUITests
+@end
